d1b428392473ed421c7e484ba026ff40eaf748df
Giselly Pereira <giselly.avp@gmail.com>
2023-04-30 13:03:05 -0300

    mudando layout mobile

--- src/components/TextoMainMob/TextoMainMob.js
+++ src/components/TextoMainMob/TextoMainMob.js
@@ -2,33 +2,27 @@ import React from 'react'
 import imgDony from '../../image/dony-brilho.png'
 import { Link } from 'react-router-dom'
 import './TextoMainMob.css'
+import logoF from '../../image/MARCA_PUJANTE-09.png'
 import AnimatedNumber from '../Header/AnimatedNumber.js';
-
 
 
 const TextoMainMob = () => {
   return (
     <div className='Mobile'>
-    <div className='text-img-mob'>
-    <div className='imagens-mob'>
-      <img src={imgDony} alt='Dony' className='imgDonyMob'/>
+    <div className='desc-evento'>
+      <p className='desc-evento-p'>O 
+      <span className='desc-evento-span'>MAIOR EVENTO DA CONSTRUÇÃO CIVIL </span>
+      REALIZADO EM <span className='desc-evento-span'>SÃO LUÍS</span></p>
     </div>
-    <div className='text-palestra-mob'>
-      <p>Não perca a oportunidade única de aprender com o <span className='p-span-mob'>Dony De Nuccio</span>, um profissional de renome 
-        na área <span className='p-span-mob'>financeira</span> e <span className='p-span-mob'>empresarial</span>, durante sua palestra. 
-        Garanta seu lugar agora mesmo e aproveite essa chance de networking!</p>
-
-       <Link className='btnLin-mob' to="/ingresso">Adquira o seu ingresso</Link>
-    </div>
-    <div className='div-extimas-num-mob'>
-          <p className='extimas-mob'>Estimativas:</p>
+    <fieldset className='div-extimas-num-mob'>
+          <legend className='extimas-mob'>ESTIMATIVA</legend>
            <div className='numbers-mob'>
             <div className='num-p-mob'>
               <span className='span-numero-mob'>
                   <p>+</p>
                   <AnimatedNumber value={70} />
               </span>
-                  <p>Expositores</p>
+                  <p className='class-desc'>Expositores</p>
             </div>
             
             <div className='num-p-mob'>
@@ -36,18 +30,38 @@ const TextoMainMob = () => {
                   <p>+</p>
                   <AnimatedNumber value={3500} />
               </span>
-                  <p>Visitantes</p>
+                  <p className='class-desc'>Visitantes</p>
             </div>
             <div className='num-p-mob'>
               <span className='span-numero-mob'>
                   <p>+</p>
                   <AnimatedNumber value={90000000} />
               </span>
-                  <p>Em negócios</p>
+                  <p className='class-desc'>Em negócios</p>
             </div>  
          </div>
-         </div>
+         </fieldset>
+    <div className='text-img-mob'>
+    
+    <div className='text-palestra-mob'>
+    <h2 className="h2-mob">PALESTRA</h2>
+    <h2 className="h2-mob">CONFIRMADA!</h2>
+      <p className='texto-p'>Não deixe de aprender com <span className='p-span-mob'>Dony De Nuccio</span>,
+      um profissional na área <span className='p-span-mob'>financeira</span> e também <span className='p-span-mob'>empresarial</span>.
+      </p>
+      <p className='temas-abr-mob'>Temas abordados:</p>
+      <p className='temas-abr-mob-2'>Comunicação, empreendorismo, economia, educação e carreira</p>
+      <div className='div-footer-mob'>
+        <p>Realização</p>
+        <img className='logo-footer-mob' src={logoF} alt='logo-footer' />
+    </div>
+    
+    </div>
+    <div className='imagens-mob'>
+      <img src={imgDony} alt='Dony' className='imgDonyMob'/>
+    </div>
   </div>
+  <Link className='btnLin-mob' to="/ingresso">Adquira o seu ingresso</Link>
   </div>
   )
 }
